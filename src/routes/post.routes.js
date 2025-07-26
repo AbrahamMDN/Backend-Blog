de0ef1830b1e3d071eb38e0c10ddc3b1b24e5002
Abraham Medina <abrahammdnlpz@gmail.com>
2025-07-25 21:38:44 -0600

    Entregable 7: Proyecto Blog, Implementación de Backend con persistencia + Robustez

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -2,18 +2,22 @@
 const express = require('express');
 const router = express.Router();
 const postController = require('../controllers/post.controller');
+const validateObjectId = require('../middleware/validateObjectId');
 
 /* Rutas y acciones */
 router.get('/', postController.getAllPost);  // endpoint = http://localhost:3000/posts/
 
-router.get('/:id', postController.getPostById);  // endpoint = http://localhost:3000/posts/:id = http://localhost:3000/posts/1
+router.get('/:id', validateObjectId('id'), postController.getPostById);  // endpoint = http://localhost:3000/posts/:id = http://localhost:3000/posts/1
+
+// Nueva ruta
+router.get('/user/:userId', postController.getPostByUserId);  // endpoint = http://localhost:3000/posts/user/:userId = http://localhost:3000/posts/user/1
 
 // Nueva ruta 
 router.get('/title/:title', postController.getPostByTitle); // endpoint = http://localhost:3000/posts/title/:title
 
 router.post('/', postController.createPost); // endpoint = http://localhost:3000/posts/
 
-router.put('/:id', postController.updatePost); // endpoint = http://localhost:3000/posts/:id = http://localhost:3000/posts/1
+router.put('/:id', validateObjectId('id'), postController.updatePost); // endpoint = http://localhost:3000/posts/:id = http://localhost:3000/posts/1
 
 router.delete('/:id', postController.deletePost); // endpoint = http://localhost:3000/posts/:id = http://localhost:3000/posts/1
 
